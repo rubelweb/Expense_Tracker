8252242759dd6f54791dd9653aa6193721ff6e78
SayedRahman <rubelweb@hotmail.com>
2024-04-01 03:03:35 +0200

    Add new expense and calculate total amount

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
+import { useState } from 'react'
 import './App.css'
 import ExpenseForm from './components/ExpenseForm/ExpenseForm'
+import { useEffect } from 'react'
 
 function App() {
+    const [expenses, setExpenses] = useState([])
+    const [totalExpense, setTotalExpense] = useState(0)
+
+    useEffect(() => {
+        const total = expenses.reduce((acc, curr) => acc + curr.amount, 0)
+        setTotalExpense(total)
+    }, [expenses])
+
+    const addExpense = (expense) => {
+        setExpenses([...expenses, expense])
+    }
+
+    console.log(JSON.stringify(expenses))
+
     return (
         <>
             <div>
                 <h1>Expense Tracker</h1>
-                <ExpenseForm />
-                <h2>Total Expense:</h2>
+                <ExpenseForm onAddExpense={addExpense} />
+                <h2>Total Expense: {totalExpense}</h2>
             </div>
         </>
     )
--- src/components/ExpenseForm/ExpenseForm.jsx
+++ src/components/ExpenseForm/ExpenseForm.jsx
@@ -1,13 +1,52 @@
-function ExpenseForm() {
+import { useState } from 'react'
+import PropTypes from 'prop-types'
+
+function ExpenseForm({ onAddExpense }) {
+    const [amount, setAmount] = useState('')
+    const [description, setDescription] = useState('')
+    const [nextId, setNextId] = useState(1)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        const newExpense = {
+            id: nextId,
+            amount: parseFloat(amount),
+            description,
+            date: new Date().toDateString()
+        }
+
+        onAddExpense(newExpense)
+
+        setAmount('')
+        setDescription('')
+        setNextId(nextId + 1)
+    }
     return (
         <div>
-            <form>
-                <input type="string" placeholder="Description" required></input>{' '}
-                <input type="number" placeholder="Amount" required></input>{' '}
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="string"
+                    value={description}
+                    placeholder="Description"
+                    onChange={(e) => setDescription(e.target.value)}
+                    required
+                ></input>{' '}
+                <input
+                    type="number"
+                    value={amount}
+                    placeholder="Amount"
+                    onChange={(e) => setAmount(e.target.value)}
+                    required
+                ></input>{' '}
                 <button type="submit">Add Expense</button>
             </form>
         </div>
     )
+}
+
+ExpenseForm.propTypes = {
+    onAddExpense: PropTypes.func.isRequired
 }
 
 export default ExpenseForm
